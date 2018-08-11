c00fd5f54805ef4ac1390ab9a887dbe784d3bfa8
萬俊瑋 <wei840222@gmail.com>
2018-08-11 13:41:17 +0800

    update meal layout

--- pages/meal.vue
+++ pages/meal.vue
@@ -13,6 +13,7 @@
           <small class="text-muted">2018/8/26 11:30~13:00</small>
         </div>
       </b-card>
+      <br/>
       <b-card title="晚宴餐券" :img-src="qrCodeUrlDinner" img-top v-if="now <= config.dinner.end && now >= config.dinner.start">
         <p class="card-text">渴望會館-B1-渴望餐廳</p>
         <div slot="footer">
@@ -25,6 +26,7 @@
           <small class="text-muted">2018/8/26 18:30~20:30</small>
         </div>
       </b-card>
+      <br/>
       <b-card title="餐盒券" :img-src="qrCodeUrlLunchBox2" img-top v-if="now <= config.lunchBox2.end && now >= config.lunchBox2.start">
         <p class="card-text">渴望會館-2F-國際會議廳-外廳</p>
         <div slot="footer">
